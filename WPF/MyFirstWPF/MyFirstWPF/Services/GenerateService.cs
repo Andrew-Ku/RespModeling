1037ecb352b7279aa7203033fc6ec3a17eb69556
Andrew-Ku <andrey.kulnev@api.nnov.ru>
2016-03-13 19:11:13 +0400

    Уход в матрицы и процедуры

--- WPF/MyFirstWPF/MyFirstWPF/Services/GenerateService.cs
+++ WPF/MyFirstWPF/MyFirstWPF/Services/GenerateService.cs
@@ -29,7 +29,7 @@ namespace MyFirstWPF.Services
             }
 
             result.Append(GetDeviceBlock());
-            result.Append(GetHelpfulBlock(model));
+          //  result.Append(GetHelpfulBlock(model));
             result.Append(GetProcedureBlock(model));
 
             File.WriteAllText(path, result.ToString());
@@ -56,20 +56,24 @@ namespace MyFirstWPF.Services
             }
 
             result.AppendLine("");
-            result.AppendLine("TimeStateMat MATRIX ,1,2; матрица для хранения времени и следующего состояния");
+            result.AppendLine("NextStateMat MATRIX ,1,2; матрица для хранения времени и следующего состояния");
+
+            result.AppendLine(string.Format("StateTimeMat MATRIX ,1,{0}; матрица для хранения времени и следующего состояния", model.Nodes.Count()));
+            result.AppendLine(string.Format("StateCountMat MATRIX ,1,{0}; матрица для хранения времени и следующего состояния", model.Nodes.Count()));
+
             result.AppendLine("initial x$workTimeAll,0");
             result.AppendLine("initial x$notWorkTimeAll,0");
             result.AppendLine("initial x$kGotov,0 ; коэффициент готовности");
-            result.AppendLine("initial x$CorrectStateMet,0 ;Метка для коррекции");
+            result.AppendLine("initial x$CorrectStateId,0 ;Метка для коррекции");
             result.AppendLine("initial x$IsCorrect,0 ; нужна ли коррекция ");
             result.AppendLine("initial x$kGotov,0 ; коэффициент готовности");
 
-            result.AppendLine("");
-            foreach (var node in model.Nodes)
-            {
-                result.AppendLine(string.Format("initial x$StateTime{0},0 ; время в состояние {0}", node.Id));
-                result.AppendLine(string.Format("initial x$StateCount{0},0 ; число заходов с состояние {0}", node.Id));
-            }
+            //result.AppendLine("");
+            //foreach (var node in model.Nodes)
+            //{
+            //    result.AppendLine(string.Format("initial x$StateTime{0},0 ; время в состояние {0}", node.Id));
+            //    result.AppendLine(string.Format("initial x$StateCount{0},0 ; число заходов с состояние {0}", node.Id));
+            //}
 
             result.AppendLine("");
 
@@ -125,35 +129,38 @@ namespace MyFirstWPF.Services
             return result.ToString();
         }
 
-        private static string GetHelpfulBlock(GpssInputModel model)
-        {
-            var result = new StringBuilder();
-            var sumString = new StringBuilder();
 
-            result.AppendLine(";--Вспомогательный блок--------------------");
-
-            foreach (var node in model.Nodes)
-            {
-                sumString.Append(string.Format("x$StateTime{0}+", node.Id));
-            }
-            sumString.Remove(sumString.Length - 1, 1);
-
-            foreach (var node in model.Nodes)
-            {
-                result.AppendLine(string.Format("CorrectState{0}Met SAVEVALUE StateTime{0}+,(Time - ({1}))", node.Id, sumString));
-                result.AppendLine(string.Format("TRANSFER ,FinishMet"));
-            }
-
-            result.AppendLine(";---------------------------------------------------------------");
-
-            return result.ToString();
-        }
+        //
+//        private static string GetHelpfulBlock(GpssInputModel model)
+//        {
+//            var result = new StringBuilder();
+//            var sumString = new StringBuilder();
+//
+//            result.AppendLine(";--Вспомогательный блок--------------------");
+//
+//            foreach (var node in model.Nodes)
+//            {
+//                sumString.Append(string.Format("MX$StateTimeMat(1,{0})+", node.Id+1));
+//            }
+//            sumString.Remove(sumString.Length - 1, 1);
+//
+//            foreach (var node in model.Nodes)
+//            {
+//                result.AppendLine(string.Format("CorrectState{0}Met MSAVEVALUE StateTimeMat+,1,{2},(Time - ({1}))", node.Id, sumString, node.Id + 1));
+//                result.AppendLine(string.Format("TRANSFER ,FinishMet"));
+//            }
+//
+//            result.AppendLine(";---------------------------------------------------------------");
+//
+//            return result.ToString();
+//        }
 
         private static string GetStateBlock(Node node)
         {
             var result = new StringBuilder();
+            var relationCount = node.NodeRelations.Count();
             result.AppendLine(string.Format(";--Состояние {0}-----------------------", node.Id));
-          //  result.AppendLine(string.Format("State{0}Met ASSEMBLE 1", node.Id));
+            //  result.AppendLine(string.Format("State{0}Met ASSEMBLE 1", node.Id));
 
             foreach (var relation in node.NodeRelations)
             {
@@ -165,27 +172,15 @@ namespace MyFirstWPF.Services
                 {
                     result.AppendLine(string.Format("SAVEVALUE LambdaTime{0}_{1},(Exponential(1,0,1/Lambda{0}_{1}))", node.Id, relation.NodeId));
                 }
-                
+
             }
-
-            var firstRelation = node.NodeRelations.FirstOrDefault();
-            var relationCount = node.NodeRelations.Count();
-
-            //if (firstRelation != null)
-            //{
-            //    result.AppendLine(string.Format("ASSIGN State,State{0}Met", firstRelation.NodeId));
-            //    result.AppendLine(string.Format("ASSIGN Time,x$LambdaTime{0}_{1}", node.Id, firstRelation.NodeId));
-            //}
 
             if (node.IsRejectionNode)
             {
                 result.AppendLine(string.Format("ASSIGN ReturnState,ReturnState{0}Met", node.Id));
             }
 
-
             var procArgs = new StringBuilder();
-
-
             for (var i = 0; i < maxRelationCount; i++)
             {
                 if (i < relationCount)
@@ -199,19 +194,18 @@ namespace MyFirstWPF.Services
                 }
             }
 
-
             result.AppendLine(string.Format("SAVEVALUE procHelpfulPar,(ChooseWayProc({0}))", procArgs.Remove(procArgs.Length - 1, 1)));
-            result.AppendLine(string.Format("ASSIGN Time,MX$TimeStateMat(1,1)"));
-            result.AppendLine(string.Format("ASSIGN State,MX$TimeStateMat(1,2)"));
+            result.AppendLine(string.Format("ASSIGN Time,MX$NextStateMat(1,1)"));
+            result.AppendLine(string.Format("ASSIGN State,MX$NextStateMat(1,2)"));
 
-            result.AppendLine(string.Format("SAVEVALUE CorrectStateMet,CorrectState{0}Met", node.Id));
-           // result.AppendLine(string.Format("SAVEVALUE IsCorrect,True"));
-            result.AppendLine(string.Format("SAVEVALUE StateCount{0}+,1", node.Id));
+            result.AppendLine(string.Format("SAVEVALUE CorrectStateId,{0}", node.Id));
+            // result.AppendLine(string.Format("SAVEVALUE IsCorrect,True"));
+            result.AppendLine(string.Format("MSAVEVALUE StateCountMat+,1,{0},1", node.Id + 1));
 
             result.AppendLine(node.IsRejectionNode ? string.Format("TRANSFER ,DevMet") : string.Format("ADVANCE p$Time"));
-          //  result.AppendLine(node.IsRejectionNode ? string.Format("ReturnState{0}Met SAVEVALUE IsCorrect,False", node.Id) : string.Format("SAVEVALUE IsCorrect,False"));
-            result.AppendLine(node.IsRejectionNode ? string.Format("ReturnState{0}Met SAVEVALUE StateTime{0}+,p$Time", node.Id) : string.Format("SAVEVALUE StateTime{0}+,p$Time",node.Id));
-          
+            //  result.AppendLine(node.IsRejectionNode ? string.Format("ReturnState{0}Met SAVEVALUE IsCorrect,False", node.Id) : string.Format("SAVEVALUE IsCorrect,False"));
+            result.AppendLine(node.IsRejectionNode ? string.Format("ReturnState{0}Met MSAVEVALUE StateTimeMat+,1,{1},p$Time", node.Id, node.Id + 1) : string.Format("MSAVEVALUE StateTimeMat+,1,{0},p$Time", node.Id + 1));
+
             result.AppendLine(string.Format("TRANSFER ,p$State"));
             result.AppendLine(";---------------------------------------------------------------");
 
@@ -227,33 +221,33 @@ namespace MyFirstWPF.Services
 
             foreach (var node in model.Nodes)
             {
-                sumString.Append(string.Format("x$StateTime{0}+", node.Id));
+                sumString.Append(string.Format("MX$StateTimeMat(1,{0})+", node.Id + 1));
             }
             sumString.Remove(sumString.Length - 1, 1);
 
             foreach (var node in model.Nodes.Where(n => !n.IsRejectionNode))
             {
-                sumWork.Append(string.Format("x$StateTime{0}+", node.Id));
+                sumWork.Append(string.Format("MX$StateTimeMat(1,{0})+", node.Id + 1));
             }
             sumWork.Remove(sumWork.Length - 1, 1);
 
             foreach (var node in model.Nodes.Where(n => n.IsRejectionNode))
             {
-                sumNotWork.Append(string.Format("x$StateTime{0}+", node.Id));
+                sumNotWork.Append(string.Format("MX$StateTimeMat(1,{0})+", node.Id + 1));
             }
             sumNotWork.Remove(sumNotWork.Length - 1, 1);
 
             result.AppendLine(";--Конечный блок--------------------");
             result.AppendLine(string.Format("GENERATE Time"));
-           // result.AppendLine(string.Format("TEST E x$IsCorrect,True,FinishMet"));
-            result.AppendLine(string.Format("TRANSFER ,x$CorrectStateMet"));
+            // result.AppendLine(string.Format("TEST E x$IsCorrect,True,FinishMet"));
+            result.AppendLine(string.Format("SAVEVALUE correctTime,(CorrectStateTime(x$CorrectStateId,C1))"));
             result.AppendLine(string.Format("FinishMet SAVEVALUE TimeAll,({0})", sumString));
             result.AppendLine(string.Format("SAVEVALUE notWorkTimeAll,({0})", sumNotWork));
             result.AppendLine(string.Format("SAVEVALUE workTimeAll,({0})", sumWork));
 
             foreach (var node in model.Nodes)
             {
-                result.AppendLine(string.Format("SAVEVALUE prob{0},(x$StateTime{0}/x$TimeAll)", node.Id));
+                result.AppendLine(string.Format("SAVEVALUE prob{0},(MX$StateTimeMat(1,{1})/x$TimeAll)", node.Id, node.Id + 1));
             }
 
             result.AppendLine(string.Format("SAVEVALUE kGotov,(x$workTimeAll/(x$notWorkTimeAll+x$workTimeAll))"));
@@ -261,14 +255,14 @@ namespace MyFirstWPF.Services
             var sumCountWork = new StringBuilder();
             foreach (var node in model.Nodes.Where(n => !n.IsRejectionNode))
             {
-                sumCountWork.Append(string.Format("x$StateCount{0}+", node.Id));
+                sumCountWork.Append(string.Format("MX$StateCountMat(1,{0})+", node.Id + 1));
             }
             sumCountWork.Remove(sumCountWork.Length - 1, 1);
 
             var sumCountNotWork = new StringBuilder();
             foreach (var node in model.Nodes.Where(n => n.IsRejectionNode))
             {
-                sumCountNotWork.Append(string.Format("x$StateCount{0}+", node.Id));
+                sumCountNotWork.Append(string.Format("mx$StateCountMat(1,{0})+", node.Id + 1));
             }
             sumCountNotWork.Remove(sumCountNotWork.Length - 1, 1);
 
@@ -294,8 +288,8 @@ namespace MyFirstWPF.Services
             var procArgs = new StringBuilder();
             for (var i = 0; i < maxRelationCount; i++)
             {
-               procArgs.Append(string.Format("timeArg{0},nextStateArg{0},", i));
-               
+                procArgs.Append(string.Format("timeArg{0},nextStateArg{0},", i));
+
             }
 
             result.AppendLine(string.Format("PROCEDURE ChooseWayProc({0})", procArgs.Remove(procArgs.Length - 1, 1)));
@@ -308,13 +302,30 @@ namespace MyFirstWPF.Services
                 result.AppendLine(string.Format("IF (minTime>timeArg{0}) THEN BEGIN minTime = timeArg{0}; nextState = nextStateArg{0}; END;", i));
 
             }
-            result.AppendLine(string.Format("TimeStateMat[1,1] = minTime; TimeStateMat[1,2] = nextState;"));
+            result.AppendLine(string.Format("NextStateMat[1,1] = minTime; NextStateMat[1,2] = nextState;"));
             result.AppendLine(string.Format("Return (nextState);"));
             result.AppendLine(string.Format("END;"));
             result.AppendLine(";--------------------------------------------------------------");
+            result.AppendLine("");
+
+            result.AppendLine(";--Процедура для коррекции времени ----------------");
+            result.AppendLine(string.Format("PROCEDURE CorrectStateTime(currentStateArg, endTimeArg)"));
+            result.AppendLine(string.Format("BEGIN"));
+            result.AppendLine(string.Format("TEMPORARY countState,result;"));
+            result.AppendLine(string.Format("countState = 1;"));
+            result.AppendLine(string.Format("sumStateTimes = 0;"));
+            result.AppendLine(string.Format("WHILE (countState <= {0}) DO BEGIN", model.Nodes.Count()));
+            result.AppendLine(string.Format("sumStateTimes = sumStateTimes + StateTimeMat[1,countState];"));
+            result.AppendLine(string.Format("countState = countState+1;"));
+            result.AppendLine(string.Format("END;"));
+            result.AppendLine(string.Format("StateTimeMat[1, currentStateArg + 1] = StateTimeMat[1, currentStateArg + 1] + endTimeArg-sumStateTimes;"));
+            result.AppendLine(string.Format(" Return(StateTimeMat[1, currentStateArg + 1]);"));
+            result.AppendLine(string.Format("END;"));
+            result.AppendLine(";--------------------------------------------------------------");
+            result.AppendLine("");
 
             return result.ToString();
         }
-        
+
     }
 }
